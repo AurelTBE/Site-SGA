2725be2e725b263e811b3676d1bbbec95c804c5c
AurelTBE <aureltrouble@gmail.com>
2019-02-10 11:22:30 +0100

    Added CustomTheme

--- components/Layout.js
+++ components/Layout.js
@@ -4,9 +4,26 @@ import React, {Fragment} from 'react'
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';  
 
 // Elements
 import HeaderDrawer from './HeaderDrawer';
+
+const theme = createMuiTheme({
+  palette: {
+     primary: {
+        light: '#ff6659',
+        main: '#d32f2f',
+        dark: '#9a0007'
+     },
+     secondary: {
+       main: '#ffd740',
+     },
+  },
+  typography: { 
+     useNextVariants: true
+  }
+});
 
 const styles = theme => ({
   root: {
@@ -18,7 +35,7 @@ function Layout(props) {
   const { classes } = props;
 
   return (
-    <Fragment>
+    <MuiThemeProvider theme = { theme }>
       <HeaderDrawer />
       <div className={classes.root}>
         <Grid container spacing={24}>
@@ -27,7 +44,7 @@ function Layout(props) {
           </Grid>
         </Grid>
       </div>
-    </Fragment>
+    </MuiThemeProvider>
   )
 }
 
