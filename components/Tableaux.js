c3b7203e7476b4d57fcc25e6659e074919d987fd
AurelTBE <aurelien.trouble@gmail.com>
2019-03-07 00:56:43 +0100

    Start complexe table

--- components/Tableaux.js
+++ components/Tableaux.js
@@ -3,7 +3,27 @@ import HtmlTableToJson from 'html-table-to-json'
 import MUIDataTable from "mui-datatables";
 
 
-export default class Tableau extends Component {
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const styles = theme => ({
+    root: {
+      width: '100%',
+      marginTop: theme.spacing.unit * 3,
+      overflowX: 'auto',
+    },
+    table: {
+      minWidth: 700,
+    },
+  });
+
+class Tableau extends Component {
     state = {data: null}
 
     componentDidMount() {
@@ -15,30 +35,67 @@ export default class Tableau extends Component {
     }
 
     render() {
+        
+        const { classes } = this.props;
         if (this.state.data === null) {
             return <div>Chargement des résultats</div>
           } else {
             const header = this.state.header
             const subHeader = this.state.subHeader
             const data = this.state.data
-        
-            const options = {
-                selectableRows: false,
-                pagination: false,
-                search: false,
-                print: false,
-                filter: false,
-                responsive: "scroll",
-            }
     
             return (
             <div>
                 {console.log(header)}
                 {console.log(subHeader)}
                 {console.log(data)}
+
+                <Paper className={classes.root}>
+                    <Table className={classes.table}>
+                        <TableHead>
+                        <TableRow>
+                            {
+                                header.map(cell => (
+                                    <TableCell key={cell}>{cell}</TableCell>
+                                ))
+                            }
+                        </TableRow>
+                        </TableHead>
+                        <TableBody>
+                        {data.map(row => (
+                            <TableRow key={row}>
+                            <TableCell>xx</TableCell>
+                            <TableCell align="right">xx</TableCell>
+                            <TableCell align="right">xx</TableCell>
+                            <TableCell align="right">xx</TableCell>
+                            </TableRow>
+                        ))}
+                        <TableRow>
+                            <TableCell rowSpan={3} />
+                            <TableCell colSpan={2}>Subtotal</TableCell>
+                            <TableCell align="right">xx</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>Tax</TableCell>
+                            <TableCell align="right">xx</TableCell>
+                            <TableCell align="right">xx</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell colSpan={2}>Total</TableCell>
+                            <TableCell align="right">xx</TableCell>
+                        </TableRow>
+                        </TableBody>
+                    </Table>
+                </Paper>
+
             </div>
             )
         }
     }
 }
 
+Tableau.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Tableau)
